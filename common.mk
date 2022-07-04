005b494efb2181bf20f6e5da8bdb9b79c4566301
Dmitrii <bankersenator@gmail.com>
2022-07-04 07:25:13 +0000

    sm8150-common: drop devicesettings
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- common.mk
+++ common.mk
@@ -85,10 +85,6 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# YAAP Device Settings
-PRODUCT_PACKAGES += \
-    DeviceSettings
-
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
