499e4ab9b4bbf5824be28b67b8650cf14773695b
Dmitrii <bankersenator@gmail.com>
2022-07-13 03:55:17 +0000

    sm8150-common: build livedisplay
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- common.mk
+++ common.mk
@@ -389,6 +389,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.1-service.oneplus_msmnile
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oneplus
